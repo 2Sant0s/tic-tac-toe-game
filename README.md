5a2dae172b1f34929e01a91974ed21899c4e2386
Josemar dos Santos <Josemardsantoss@outlook.com>
2025-06-18 11:49:50 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,44 @@
+<h1 align="center" style="font-weight: bold;">Tic Tac Toe </h1>
+
+<br>
+<p align="center">
+ <a href="#tech" title="technologies">Technologies</a> • 
+ <a href="#about" title="about">About</a> • 
+ <a href="#clone" title="clone">Clone</a> •
+ <a href="https://github.com/2Sant0s/form-login-tecnotrends/blob/main/index.html">Project</a>
+</p>
+
+<h2 id="layout">🎨 Layout</h2>
+
+<p align="center">
+    <img src="images/tictactoe.gif" width="700px">
+</p>
+
+<h2 id="tech">💻 Technologies</h2>
+
+[![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)](https://developer.mozilla.org/en-US/docs/Web/HTML)
+[![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)](https://developer.mozilla.org/en-US/docs/Web/CSS)
+[![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+<br>
+
+
+<!-- about -->
+<h2 id="about">☁️ About</h2>
+<p>Tic-tac-toe game built with HTML, CSS and JavaScript for learning/practice.
+</p>
+
+<!-- sobre -->
+<h2 id="clone">📥 Clone</h3>
+
+To get a copy of this project, open your terminal and run:
+
+```bash
+git clone https://github.com/2Sant0s/tic-tac-toe-game.git
+```
+
+<h2 id="status">🚦 Status</h2>
+
+<!-- ![Status: Finished](https://img.shields.io/badge/status-finished-brightgreen) -->
+Experimental project, may contain bugs
+
+![Experimental](https://img.shields.io/badge/status-experimental-red)
---
+++ script.js
@@ -0,0 +1,89 @@
+var divElement = document.querySelector('div');
+var tableElement = document.querySelector('table');
+
+var Game = {
+	start() {
+		this.field = [
+			['', '', ''],
+			['', '', ''],
+			['', '', '']
+		];
+
+		this.currentPlayer = '❌';
+		this.isFinished = false;
+		this.round = 0;
+		this.render();
+	},
+	nextPlayer() {
+		this.currentPlayer = this.currentPlayer === '❌' ? '⚫' : '❌';
+	},
+	setField(line, column) {
+		 if(!this.isFinished && this.field[line][column] === '') {
+		 	this.field[line][column] = this.currentPlayer;
+		 	this.nextPlayer();
+		 	this.round++;
+		 	this.render();
+		 }
+	},
+	isGameOver() {
+		var field = this.field,
+		rows = 3,
+		cols = 3,
+		totalRow = 0,
+		totalCol = 0;
+		for(var i = 0; i < rows; i++) {
+			totalRow = 0;
+			totalCol = 0;
+
+			for(var j = 0; j < cols; j++) {
+				if(field[i][j] === '❌') {
+					totalRow++;
+				}
+				if(field[i][j] === '⚫') {
+					totalRow--;
+				}
+				if(field[j][i] === '❌') {
+					totalCol++;
+				}
+				if(field[j][i] === '⚫') {
+					totalCol--;
+				}
+			}
+			if(totalRow === 3 || totalCol === 3) {
+				return '❌'
+			}
+			if(totalRow === -3 || totalCol === -3) {
+				return '⚫'
+			}
+		}
+		if(field[0][0] !== '' && field[0][0] === field[1][1] && field[1][1] === field[2][2]) {
+			return field[0][0];
+		}
+		if(field[0][2] !== '' && field[0][2] === field[1][1] && field[1][1] === field[2][0]) {
+			return field[0][2];
+		}
+		if(this.round === rows * cols) {
+			return 'Ninguém...';
+		}
+
+	},
+	render() {
+		var winner = this.isGameOver();
+		divElement.textContent = winner ? `Vencedor: ${winner}` : `Jogador: ${this.currentPlayer}`;
+
+		if(winner) {
+			this.isFinished = true;
+		}
+		var template = '';
+		this.field.forEach((line, lineIndex)=> {
+			template += '<tr>';
+			line.forEach((column, columnIndex) => {
+				template += `<td onclick="Game.setField(${lineIndex}, ${columnIndex})">${column}</td>`;
+			})
+			template += '</tr>'
+		})
+		tableElement.innerHTML = template;
+	}
+}
+
+Game.start();
